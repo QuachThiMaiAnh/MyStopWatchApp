582961af24af712c691f18e1548f6f76a546939c
QuachThiMaiAnh <quachthimaianh2002@gmail.com>
2024-04-13 09:08:37 +0700

    second

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StyleSheet, View } from "react-native";
 import StopWatch from "./components/Stopwatch";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StopWatch />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StopWatch />
+      </View>
+    </SafeAreaProvider>
   );
 }
 const styles = StyleSheet.create({
